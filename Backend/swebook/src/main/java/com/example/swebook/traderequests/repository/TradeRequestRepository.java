c18a765c68de8edc0dc8ab40e8740f62043f47c5
Lidohyeon <ehgus3193@naver.com>
2026-06-04 01:39:46 +0900

    feat: trade-requests 도메인 구조 확립 및 조회 api 생성

---
+++ Backend/swebook/src/main/java/com/example/swebook/traderequests/repository/TradeRequestRepository.java
@@ -0,0 +1,13 @@
+package com.example.swebook.traderequests.repository;
+
+import com.example.swebook.traderequests.entity.TradeRequest;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TradeRequestRepository extends JpaRepository<TradeRequest, Long> {
+
+    @EntityGraph(attributePaths = {"user", "tradePost", "tradePost.book", "tradePost.seller", "availableTime"})
+    Optional<TradeRequest> findWithDetailsByRequestId(Long requestId);
+}
